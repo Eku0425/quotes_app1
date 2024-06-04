605e58dfcf71796b7089010b2f4a33b352066a8d
EKTA TRIVEDI <ekutrivedi2512@>
2024-06-04 12:46:31 +0530

    4/6/2024

---
+++ lib/Radam_screen/Listview.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:quotes_appp/utils/golbalList.dart';
+
+import '../utils/quoteModal.dart';
+import 'HomePage.dart';
+
+class ListViewScreen extends StatefulWidget {
+  const ListViewScreen({super.key});
+
+  @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
+  void initState() {
+    quoteModel = QuoteModel.toList(QutoesList);
+    super.initState();
+  }
+  bool en = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text(
+          'Quotes App',
+          style: TextStyle(color: Colors.white,fontSize: 25),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: ActionChip(
+              backgroundColor: Colors.blueGrey,
+              side: BorderSide(color: Colors.blueGrey),
+              label: const Text(''),
+              avatar: Icon(
+                en ? Icons.grid_view_outlined : Icons.list,
+                size: 30,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  en = !en;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+      body: (en)
+          ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 9 / 8),
+              itemBuilder: (context, index) => ListTile(
+                title: Text(quoteModel!.quoteModelList[index].quote!),
+                subtitle: Text(quoteModel!.quoteModelList[index].author!),
+              ),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) => ListTile(
+                title: Text(quoteModel!.quoteModelList[index].quote!),
+                subtitle: Text(quoteModel!.quoteModelList[index].author!),
+              ),
+            ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:quotes_appp/Radam_screen/HomePage.dart';
+import 'package:quotes_appp/Radam_screen/Listview.dart';
 
 
 class AppRoutes
 {
   static Map<String, Widget Function(BuildContext)> routes =
   {
-    '/': (context) => HomePageScren(),
+     '/':(context)=>ListViewScreen(),
+    '/home': (context) => HomePageScren(),
   };
 }
